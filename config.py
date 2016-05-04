fa89ddd0f6785f19cee4affaf69aad8770f6d702
Fantom <fantom@dhcppc0>
2016-05-04 13:01:30 +0200

    DB init

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     CSRF_ENABLED = True
     SECRET_KEY = 'Arafa@2016'
 
