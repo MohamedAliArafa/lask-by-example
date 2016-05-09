b53debd576137bc8141796d3e4953bb53207047a
Fantom <fantom@dhcppc0>
2016-05-09 21:43:56 +0200

    GCM

--- app.py
+++ app.py
@@ -356,7 +356,7 @@ def make_order():
             quantity = req_json['quantity']
             item = db.session.query(models.Items).filter_by(id=item_id).one()
             user = db.session.query(models.User).filter_by(id=user_id).one()
-            order = db.session.query(models.User).filter_by(user_id=user_id, item_id=item_id)
+            order = db.session.query(models.Orders).filter_by(user_id=user_id, item_id=item_id)
             if order is None:
                 order = models.Orders(user=user, item=item, quantity=quantity)
             else:
@@ -365,7 +365,7 @@ def make_order():
                 db.session.commit()
             db.session.add(order)
             db.session.commit()
-            
+
             return jsonify(response=1)
         else:
             return jsonify(response=-1)
